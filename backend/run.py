881517ab77658caf1f8942d275c3ac539934b2ef
junhaoyu <yu1791046157@gmail.com>
2025-12-26 10:28:49 +0800

    update

--- backend/run.py
+++ backend/run.py
@@ -12,4 +12,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=5001) 
